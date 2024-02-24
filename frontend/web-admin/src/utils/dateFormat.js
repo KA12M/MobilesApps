3dca55159871b9f29ec756ce3f789d688691accc
Kaewkwan <sanung2002@gmail.com>
2024-02-24 14:20:00 +0700

    [Fix] - Remove project vue

--- frontend/web-admin/src/utils/dateFormat.js
+++
@@ -1,6 +0,0 @@
-export function formatISODateToThaiDate(isoDate) {
-  const date = new Date(isoDate);
-  const options = { year: "numeric", month: "long", day: "numeric" };
-  const thaiDate = date.toLocaleDateString("th-TH", options);
-  return thaiDate;
-}
