1c4b12f17dca5dd393ee7d3fb30d1263140520cd
Kaewkwan <sanung2002@gmail.com>
2024-03-01 02:05:55 +0700

    [Fix] - remove user
    backend

--- src/API/Controllers/UserController.cs
+++ src/API/Controllers/UserController.cs
@@ -2,12 +2,21 @@
 using Application.Core;
 using Application.Users;
 using Application.Users.DTOs;
+using Domain;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
 public class UserController : BaseApiController
 {
+    private readonly DataContext _context;
+
+    public UserController(DataContext context)
+    {
+        _context = context;
+    }
+
     [HttpGet]
     public async Task<ActionResult> GetUsers([FromQuery] PagingParams param)
     {
@@ -31,4 +40,19 @@ public class UserController : BaseApiController
     {
         return HandleResult(await Mediator.Send(new EditUser.Command { User = user }));
     }
+
+    [HttpDelete]
+    public async Task<ActionResult> RemoveUser(int userId)
+    {
+        var user = await _context.Users.FirstOrDefaultAsync(x => x.Id.Equals(userId));
+
+        if (user is null) return StatusCode(StatusCodes.Status404NotFound);
+
+        _context.Users.Remove(user);
+
+        return Ok(await _context.SaveChangesAsync() > 0 
+            ? StatusCode(StatusCodes.Status200OK) 
+            : StatusCode(StatusCodes.Status400BadRequest));
+    }
+
 }
